8caf9c9163d3f037dd42556e73e2aa893396e866
Yufeng Zhang <kinginsun@163.com>
2016-01-10 17:23:02 +0800

    up

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,13 +2,13 @@
 lock '3.1.0'
 
 set :application, 'testapp'
-set :repo_url, 'git@example.com:me/my_repo.git'
+set :repo_url, 'git@github.com:kinginsun/testapp.git'
 
 # Default branch is :master
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
 
 # Default deploy_to directory is /var/www/my_app
-# set :deploy_to, '/var/www/my_app'
+set :deploy_to, '/home/deploy/testapp'
 
 # Default value for :scm is :git
 # set :scm, :git
@@ -23,10 +23,10 @@ set :repo_url, 'git@example.com:me/my_repo.git'
 # set :pty, true
 
 # Default value for :linked_files is []
-# set :linked_files, %w{config/database.yml}
+set :linked_files, %w{config/database.yml}
 
 # Default value for linked_dirs is []
-# set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
@@ -40,12 +40,12 @@ namespace :deploy do
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
       # Your restart mechanism here, for example:
-      # execute :touch, release_path.join('tmp/restart.txt')
+      execute :touch, release_path.join('tmp/restart.txt')
     end
   end
 
-  after :publishing, :restart
-
+  after :publishing, 'deploy:restart'
+  after :finishing,'deploy:cleanup'
   after :restart, :clear_cache do
     on roles(:web), in: :groups, limit: 3, wait: 10 do
       # Here we can do anything such as:
